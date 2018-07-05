6a32e2304b41538e35636b3406b653f9967f7c6a
Jan Nold <jan.nold@udo.edu>
2018-07-05 16:55:19 +0200

    correction of signal strength calculation etc

--- signal_strength_visualizer.py
+++ signal_strength_visualizer.py
@@ -1,31 +1,38 @@
 import paho.mqtt.client as mqtt
 import matplotlib.pyplot as plt
+import matplotlib as mtp
+mtp.use("TkAgg")
 import math
 import json
 import copy
+import numpy as np
 # position of antenna in the vicon coordinate system
 antenna_positions = {
-    "1.1": [-6.45, -18.37],
-    "1.2": [-6.45, -18.37],
-    "3.2": [13.34, 1.42],
-    "6.2": [-2.61, -14.53],
-    "6.1": [2.49, -9.43],
-    "3.1": [8.46, -3.46],
-    "2.2": [2.34, -9.58],
-    "2.1": [-2.7, -14.62],
-    "5.1": [13.46, 1.54],
-    "5.2": [8.38, -3.54],
-    "4.1": [20.45, 8.53],
-    "4.2": [20.45, 8.53]
+    "1.1": [-6.45, -2.67],
+    "1.2": [-6.45, -7.76],
+    "3.2": [13.34, -11.11],
+    "6.2": [-2.61, 0.79],
+    "6.1": [2.49, 0.79],
+    "3.1": [8.46, -11.11],
+    "2.2": [2.34, -11.11],
+    "2.1": [-2.7, -11.11],
+    "5.1": [13.46, 0.79],
+    "5.2": [8.38, 0.79],
+    "4.1": [20.45, -8.04],
+    "4.2": [20.45, -2.42]
 }
 robo_x = 0.0
 robo_y = 0.0
-arrow_start_x = 3
-arrow_start_y = 3
-arrow_end_x = 7
-arrow_end_y = 7
+# arrow_start_x = 3
+# arrow_start_y = 3
+# arrow_end_x = 7
+# arrow_end_y = 7
 normalized_directions = {}
 antennas_strength = {}
+
+plt.ion()
+# fig1 = mtp.figure.Figure('RSSI_Visualization')
+
 
 
 
@@ -34,6 +41,7 @@ def calculate_normalized_direction(robot_position):
     _normalized_directions = {}
     for key in antenna_positions:
         antenna_x, antenna_y = antenna_positions[key]
+
         # calculate difference in each dimension
         direction = [antenna_x - robot_position[0], antenna_y - robot_position[1]]
         # calculate arc length
@@ -52,15 +60,17 @@ def on_connect(client, userdata, flags, rc):
         for sub in range(1,3):
             client.subscribe("/sdr/antenna_" + str(ant) + "." + str(sub) + "/rssi")
 
-    client.subscribe("/signalStrengthPosition")
+    client.subscribe("/sdr/signalStrengthPosition")
 
 def on_message(client, userdata, msg):
+    global robo_x, robo_y
     #print "on message"
     print(msg.topic+" "+str(msg.payload))
-    if msg.topic == "/signalStrengthPosition":
+    if msg.topic == "/sdr/signalStrengthPosition":
         #print "got robot position"
         robo_x = round(json.loads(msg.payload)['x']/1000, 2)
         robo_y = round(json.loads(msg.payload)['y']/1000, 2)
+        # print('RX: ',robo_x,'RY: ',robo_y)
         calculate_normalized_direction([robo_x, robo_y])
     else:
         antenna = msg.topic[13:16]
@@ -71,15 +81,21 @@ def on_message(client, userdata, msg):
 
 def calculate_relative_signal_strength(rssi):
     # -60 bis -100
-    return min(1.0, (-60 - min(-60, rssi))/30)
+    MIN_STRENGTH = -105
+    MAX_STRENGTH = -60
+    RANGE_STRENGTH = MAX_STRENGTH - MIN_STRENGTH
+    return round((1./RANGE_STRENGTH)+((-MIN_STRENGTH + min(MAX_STRENGTH, max(MIN_STRENGTH,float(rssi))))/RANGE_STRENGTH),2)
+
 
 
 def calculate_indicator_points():
+    global robo_x,robo_y
+
     # for each antenna
     # calculate starting point: robot point + vector * some radius offset
     # calculate end point: start point + vector * signal_strength
     # publish the start/end point pairs to laser system / visualization system
-    points = {}
+    points = []
     for key in antennas_strength:
         # print('Key: ', key)
         if not key in normalized_directions.keys(): break
@@ -87,31 +103,44 @@ def calculate_indicator_points():
         arrow_start_y = round(robo_y + (normalized_directions[key][1] * 0.3),3)
 
         relative_strength = calculate_relative_signal_strength(antennas_strength[key])
+        print key, str(relative_strength)
         arrow_end_x = round(arrow_start_x + (normalized_directions[key][0] * relative_strength),3)
         arrow_end_y = round(arrow_start_y + (normalized_directions[key][1] * relative_strength),3)
-        points[key] = {"start": {"x": arrow_start_x, "y": arrow_start_y},
-                        "end": {"x": arrow_end_x, "y": arrow_end_y}}
+        points.append({"antenna":str(key),"start": {"x": arrow_start_x, "y": arrow_start_y},
+                        "end": {"x": arrow_end_x, "y": arrow_end_y}})
         print key
-        print "start: ", points[key]["start"]
-        print "end:   ", points[key]["end"]
-        plt.figure('RSSI_Visualization')
-        plt.plot((arrow_start_x, arrow_end_x),(arrow_start_y, arrow_end_y))
-        plt.plot(robo_x,robo_y,'o')
-        plt.show()
-        publish_indicator([arrow_start_x, arrow_start_y],[arrow_end_x, arrow_end_y])
+        # print "start: ", points[key]["start"]
+        # print "end:   ", points[key]["end"]
+        print ('Robo X: ',robo_x, 'Robo Y: ',robo_y)
+        # plot_visualization(robo_x,robo_y)
+        # print(points)
+        # publish_indicator([arrow_start_x, arrow_start_y],[arrow_end_x, arrow_end_y])
     publish_indicators(points)
     # print "-----"
     # print points
     # print "-----"
+def plot_visualization(robo_pos_x, robo_pos_y):
+    # global fig1
+
+    ax = fig1.add_subplot(111)
+    # ax.set_xlim(-8,8)
+    # ax.set_ylim(-5,5)
+    line1, = ax.plot(robo_pos_x, robo_pos_y, 'or')
+    line1.set_ydata(robo_pos_y)
+    line1.set_xdata(robo_pos_x)
+
+    fig1.canvas.draw()
+    # plt.close(fig1)
 
 def publish_indicators(points):
-    client.publish("/signalStrengthIndicators", json.dumps(points), qos=2)
+    client.publish("/sdr/signalStrengthIndicators", json.dumps({"data":points}), qos=2)
+    # print(json.dumps())
 
 # debugging for plot
-plt.figure('RSSI_Visualization')
-plt.plot((arrow_start_x, arrow_end_x),(arrow_start_y, arrow_end_y))
-plt.plot(robo_x,robo_y,'o')
-plt.show()
+# plt.figure('RSSI_Visualization')
+# plt.plot((arrow_start_x, arrow_end_x),(arrow_start_y, arrow_end_y))
+# plt.plot(robo_x,robo_y,'o')
+# plt.show()
 # debugging for plot
 
 client = mqtt.Client()
