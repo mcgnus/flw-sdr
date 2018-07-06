1a4660a17b71fc23a63d8476b50fe923eca83ff9
Jan Nold <jan.nold@udo.edu>
2018-07-06 12:32:18 +0200

    heatmap logger publishes data with position

--- signal_heatmap_logger.py
+++ signal_heatmap_logger.py
@@ -6,15 +6,21 @@ import numpy as np
 
 robo_x = 0.0
 robo_y = 0.0
-# arrow_start_x = 3
-# arrow_start_y = 3
-# arrow_end_x = 7
-# arrow_end_y = 7
-normalized_directions = {}
-antennas_strength = {}
 
-plt.ion()
-# fig1 = mtp.figure.Figure('RSSI_Visualization')
+antennas_strength = {
+    "1.1": 0.0,
+    "1.2": 0.0,
+    "2.1": 0.0,
+    "2.2": 0.0,
+    "3.1": 0.0,
+    "3.2": 0.0,
+    "4.1": 0.0,
+    "4.2": 0.0,
+    "5.1": 0.0,
+    "5.2": 0.0,
+    "6.1": 0.0,
+    "6.2": 0.0
+}
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -24,23 +30,18 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("/sdr/signalStrengthPosition")
 
 def on_message(client, userdata, msg):
-    global robo_x, robo_y
-    #print "on message"
-    print(msg.topic+" "+str(msg.payload))
+    global robo_x, robo_y, antennas_strength
     if msg.topic == "/sdr/signalStrengthPosition":
-        #print "got robot position"
         robo_x = round(json.loads(msg.payload)['x']/1000, 2)
         robo_y = round(json.loads(msg.payload)['y']/1000, 2)
-        # print('RX: ',robo_x,'RY: ',robo_y)
-        calculate_normalized_direction([robo_x, robo_y])
+        client.publish("/sdr/heatmap_data", json.dumps({
+            "robot_position": {"x": robo_x, "y": robo_y},
+            "antennas_strength": antennas_strength }), qos=2)
     else:
-        antenna = msg.topic[13:16]
         antennas_strength[antenna] = round(float(msg.payload), 2)
-    calculate_indicator_points()
 
 
-
-
+# register callbacks and connect to server
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
